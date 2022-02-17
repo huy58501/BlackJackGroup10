e636f2ea6c86ab7bdbcc7330004e27d70b14c8f0
Sharif Kulmie <kulmie@sheridancollege.ca>
2022-02-16 23:44:34 -0500

    Added field variables and register method to Register class

--- BlackJackGroup10/src/blackjackgroup10/Register.java
+++ BlackJackGroup10/src/blackjackgroup10/Register.java
@@ -4,10 +4,53 @@
  */
 package blackjackgroup10;
 
+import java.util.Scanner;
+
 /**
  *
  * @author chuong5850
  */
 public class Register {
+    private String name;
+    private String email;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void registerUser() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        this.name = input.nextLine();
+        System.out.print("Enter your Email: ");
+        this.email = input.nextLine();
+        System.out.print("Enter your Age: ");
+        this.age = input.nextInt();
+
+        System.out.println(name + " you are registered ");
+
+        input.close();
+    }
     
 }

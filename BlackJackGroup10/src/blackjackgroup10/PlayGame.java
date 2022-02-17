8d835072a0bb5070cb97a69f0f8f98c3a762e7c7
Sharif Kulmie <kulmie@sheridancollege.ca>
2022-02-16 23:45:12 -0500

    Added Register Object to the main method

--- BlackJackGroup10/src/blackjackgroup10/PlayGame.java
+++ BlackJackGroup10/src/blackjackgroup10/PlayGame.java
@@ -14,5 +14,7 @@ public class PlayGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Register User1 = new Register();
+        User1.registerUser();
     }
 }

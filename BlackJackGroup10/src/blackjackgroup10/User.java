83934a818684774a855de267cb33c1906af606a6
huy5801 <huy5850@gmail.com>
2022-02-17 07:36:13 -0500

    Add some variables in User class

--- BlackJackGroup10/src/blackjackgroup10/User.java
+++ BlackJackGroup10/src/blackjackgroup10/User.java
@@ -22,4 +22,36 @@ public class User {
         name = givenName;
         password=givenPass;
     }
+
+    /** The getter for the user name
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * The setter for the user name
+     * @param givenName the name to set
+     */
+    public void setName(String givenName) {
+        name = givenName;
+    }
+
+    /**
+     * The getter for the password
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+    
+    /**
+     * A setter for the password
+     * @param givenPass 
+     */
+    public void setPassword(String givenPass)
+    {
+        password = givenPass;
+    }
 }

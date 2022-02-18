6cf6cd74db90dd77202af350e90dd3c314d46c5d
huy5801 <huy5850@gmail.com>
2022-02-18 12:05:29 -0500

    add UserCheck class

---
+++ BlackJackGroup10/src/blackjackgroup10/UserCheck.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blackjackgroup10;
+
+/**
+ *
+ * @author chuong5850
+ */
+public class UserCheck {
+
+    public String passCheck(String password){
+        return password;
+    }
+    
+    public String userCheck(String user){
+        return user;
+    }
+}

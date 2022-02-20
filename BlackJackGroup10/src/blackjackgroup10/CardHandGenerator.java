d5a87124eedf2b8492dbafc1a612c2f3064cb6c6
joshua <joshsoccerstar7@gmail.com>
2022-02-19 21:01:20 -0500

    add body inside of generatorHand method

--- BlackJackGroup10/src/blackjackgroup10/CardHandGenerator.java
+++ BlackJackGroup10/src/blackjackgroup10/CardHandGenerator.java
@@ -1,15 +1,32 @@
 package blackjackgroup10;
 
+import java.util.Random;
+import blackjackgroup10.Card.Value;
+import blackjackgroup10.Card.Suit;
 
 /**
  *
  * @author joshs
  */
 public class CardHandGenerator{
-    
-        public Card GeneratorHand(int numCard){
+      
+        public static Card[] generatorHand(int numCard){
             
-                       
+            Card[] hand = new Card[numCard];
+            Random random = new Random();
+            
+            for(int i = 0;i<hand.length;i++){
+                
+                int numValues = Card.Value.values().length;
+                Card.Value value = Card.Value.values()[random.nextInt(numValues)];
+
+                int numSuits = Card.Suit.values().length;
+                Card.Suit suit = Card.Suit.values()[random.nextInt(numSuits)];
+
+                Card card = new Card(value, suit);
+                hand[i] = card;
+            }
+            return hand;
         }
     
 }

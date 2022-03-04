85f7a3a26bf6055ee546c97aca77b6268b125420
Sharif Kulmie <kulmie@sheridancollege.ca>
2022-03-04 02:27:24 -0500

    Added Login Class

---
+++ BlackJackGroup10/src/blackjackgroup10/Login.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjackgroup10;
+
+/**
+ *
+ * @author Sharif
+ */
+import java.util.*;
+public class Login {
+   
+     public void userLogin(){
+       
+    Scanner scan = new Scanner (System.in);
+    System.out.println("Enter your user name: ");
+    String enteredUser = scan.nextLine();
+    System.out.println("Enter your password: ");
+    String enteredPassword = scan.nextLine();
+    
+    //User login = new User(enteredUser, enteredPassword);
+    
+    UserCheck login = new UserCheck();
+    if(login.passCheck().equals(enteredPassword) && login.userCheck().equals(enteredUser)){
+        System.out.println("You're logged in successfully " );        
+    }   
+    else {
+        System.out.println("Icorect user name or password" );
+    }
+    
+  }
+}

4da299a32ecbd250da83b553dcdc97d560c34fa4
huy5801 <huy5850@gmail.com>
2022-04-23 01:14:02 -0400

    fix bugs

--- BlackJackGroup10/test/blackjackgroup10/RegisterTest.java
+++ BlackJackGroup10/test/blackjackgroup10/RegisterTest.java
@@ -124,7 +124,4 @@ public class RegisterTest {
         // TODO review the generated test code and remove the default call to fail.
        // fail("The test case is a prototype.");
     }
-
-   
-    
 }
